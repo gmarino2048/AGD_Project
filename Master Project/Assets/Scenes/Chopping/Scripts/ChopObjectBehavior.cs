099625e0a7c966d65df478403c826f7daf04d74e
Guy Marino <gxm262@case.edu>
2018-09-15 18:32:14 -0400

    Working on choppable object

--- Master Project/Assets/Scenes/Chopping/Scripts/ChopObjectBehavior.cs
+++ Master Project/Assets/Scenes/Chopping/Scripts/ChopObjectBehavior.cs
@@ -5,12 +5,17 @@ using UnityEngine;
 public class ChopObjectBehavior : MonoBehaviour {
 
     [Header("Sprite Information")]
+    [SerializeField]
+    public BoxCollider2D ChopObject;
+
     public float LeftBound;
     public float RightBound;
 
 	// Use this for initialization
 	void Start () {
-		
+        ChopObject = gameObject.GetComponentInChildren<BoxCollider2D>();
+
+        SetBounds(ChopObject);
 	}
 	
 	// Update is called once per frame
@@ -18,7 +23,10 @@ public class ChopObjectBehavior : MonoBehaviour {
 		
 	}
 
-    void setBounds () {
+    void SetBounds (BoxCollider2D boundaries) {
+        Vector3 position = boundaries.gameObject.transform.position;
 
+        Debug.Log(boundaries.bounds.center);
+        Debug.Log(boundaries.bounds.extents);
     }
 }
