6499c91191c7d4279ac637ba2809a914a66f2bb6
Guy Marino <gxm262@case.edu>
2018-09-15 18:20:40 -0400

    Started Knife and Chopping Behavior

---
+++ Master Project/Assets/Scenes/Chopping/Scripts/ChopObjectBehavior.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ChopObjectBehavior : MonoBehaviour {
+
+    [Header("Sprite Information")]
+    public float LeftBound;
+    public float RightBound;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void setBounds () {
+
+    }
+}
---
+++ Master Project/Assets/Scenes/Chopping/Scripts/KnifeBehavior.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class KnifeBehavior : MonoBehaviour {
+
+    [Header("Screen Information")]
+    [SerializeField]
+    public Camera Camera;
+    public float XOffset;
+    public float YOffset;
+    public float ZIndex;
+
+    [Header("Scene References")]
+    [SerializeField]
+    public Collider2D ItemToChop;
+    public float BufferWidth;
+
+	// Use this for initialization
+	void Start () 
+    {
+        // Set the inital position of the knife
+        float initialX = Camera.transform.position.x + XOffset;
+        float initialY = Camera.transform.position.y + YOffset;
+
+        Vector3 knifePosition = new Vector3(initialX, initialY, ZIndex);
+        gameObject.transform.position = knifePosition;
+	}
+	
+	// Update is called once per frame
+	void Update () 
+    {
+		
+	}
+}
