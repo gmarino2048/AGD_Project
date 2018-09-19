22ad1d6a3dbf0b5c320564e7e74fccebd8648061
Guy Marino <gxm262@case.edu>
2018-09-19 16:10:52 -0400

    Created Scorekeeper behavior script

---
+++ Master Project/Assets/Scenes/Chopping/Scripts/ScorekeeperBehavior.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ScorekeeperBehavior : MonoBehaviour
+{
+
+    #region Parameters
+
+    #endregion
+
+
+    #region MonoBehaviour
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+    #endregion
+
+    #region 
+}
