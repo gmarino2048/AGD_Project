bb5c16e68d63840f0cb78682a1df45adab73993d
Guy Marino <gxm262@case.edu>
2018-09-19 19:21:28 -0400

    Updated average distance function

--- Master Project/Assets/Scenes/Chopping/Scripts/ScorekeeperBehavior.cs
+++ Master Project/Assets/Scenes/Chopping/Scripts/ScorekeeperBehavior.cs
@@ -1,32 +1,100 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿using System.Collections.Generic;
+using System.Linq;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class ScorekeeperBehavior : MonoBehaviour
 {
 
     #region Parameters
-    [Header("Scene References")]
+
     [SerializeField]
-    public ChopManager ChopManager { get; set; }
+    public ChopManager ChopManager;
+
+    public float TimeInSeconds;
+
+    Text TimerText;
+    Button FinishButton;
+
+    float CurrentTime;
+    bool TimerActive;
+    bool TimerDone;
 
     #endregion
 
-
     #region MonoBehaviour
+
     void Start()
     {
+        TimerText = GetComponentInChildren<Text>();
+        FinishButton = GetComponentInChildren<Button>();
 
+        CurrentTime = TimeInSeconds;
+        TimerActive = true;
+        TimerDone = false;
     }
 
-    // Update is called once per frame
+
     void Update()
     {
+        if (CurrentTime > 0 && TimerActive)
+        {
+            TimerText.text = ((int)CurrentTime).ToString();
+            CurrentTime -= Time.deltaTime;
+        }
+        else if (TimerActive)
+        {
+            TimerDone = true;
+            TimerActive = false;
+        }
 
+        if (TimerDone)
+        {
+            Score();
+            TimerDone = false;
+        }
     }
+
     #endregion
 
     #region Auxiliary
+
+    float Score () {
+        float average = AverageDistance(SortChops(ChopManager));
+        Debug.Log(average);
+        return average;
+    }
+
+    List<float> SortChops (ChopManager manager){
+        List<float> chops = new List<float>();
+
+        manager.AlreadyChopped.ForEach(chop => chops.Add(chop.ActualPosition));
+
+        chops.Sort();
+
+        return chops;
+    }
+
+    float AverageDistance(List<float> values)
+    {
+        if (values.Count > 1)
+        {
+            int current = 1;
+            int total = values.Count;
+
+            float average = values[0];
+
+            while (current < total)
+            {
+                float val = values[current];
+                average = ((average * (current - 1)) + val) / current;
+                current++;
+            }
+
+            return average;
+        }
+        return 0;
+    }
 
     #endregion
 }
