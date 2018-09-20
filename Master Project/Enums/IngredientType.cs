63de342813d488dca18447e131566787094ae211
Guy Marino <gxm262@case.edu>
2018-09-19 22:24:11 -0400

    Moved Enums to Assets folder

--- Master Project/Enums/IngredientType.cs
+++
@@ -1,7 +0,0 @@
-﻿/// <summary>
-/// Used to keep track of the various ingredient types and to be used for the dish feature vector
-/// </summary>
-public enum IngredientType
-{
-    WhippedCream = 1
-}
