e3d92f65c665ae39005934195fcd462cb48f5bd8
Noah Crowley <noahwillcrow@gmail.com>
2018-09-19 21:36:14 -0400

    Added documentation

--- Master Project/Enums/IngredientType.cs
+++ Master Project/Enums/IngredientType.cs
@@ -1,4 +1,7 @@
-﻿public enum IngredientType
+﻿/// <summary>
+/// Used to keep track of the various ingredient types and to be used for the dish feature vector
+/// </summary>
+public enum IngredientType
 {
     WhippedCream = 1
 }
