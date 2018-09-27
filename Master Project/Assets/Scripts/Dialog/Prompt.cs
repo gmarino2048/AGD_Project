aaca219627cd200b34a9551539ae8426c698584e
Noah Crowley <noahwillcrow@gmail.com>
2018-09-26 23:10:42 -0400

    Created the DialogManager class for monster dialog

---
+++ Master Project/Assets/Scripts/Dialog/Prompt.cs
@@ -0,0 +1,9 @@
+﻿namespace Dialog
+{
+    public class Prompt
+    {
+        public string Body { get; set; }
+        public string NextPromptID { get; set; }
+        public string ResponseSetID { get; set; }
+    }
+}
