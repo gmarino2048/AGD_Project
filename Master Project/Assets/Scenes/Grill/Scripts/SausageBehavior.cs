88844b3826512427ea5183751c009b14be97eda6
Guy Marino <gxm262@case.edu>
2018-10-01 11:12:29 -0400

    Some changes

---
+++ Master Project/Assets/Scenes/Grill/Scripts/SausageBehavior.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SausageBehavior : MonoBehaviour {
+
+    public float LifeCycle;
+
+    [SerializeField]
+    public SpriteRenderer Renderer;
+    [SerializeField]
+    public List<Sprite> Sprites;
+
+    float TimeRemaining;
+
+	/// <summary>
+    /// Called once at start of scene
+    /// </summary>
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (TimeRemaining > 0)
+        {
+
+            TimeRemaining -= Time.deltaTime;
+        }
+    }
+
+    void Evaluate () {
+        int length = Sprites.Count;
+
+        float position = 
+    }
+
+    void SwapSprite () {
+
+    }
+}
---
+++ Master Project/Assets/Scenes/Grill/Scripts/TimerBehavior.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class TimerBehavior : MonoBehaviour {
+
+    public float InitialTime = 30f; // Set timer for 30s by default
+
+    float CurrentTime;
+    Text Text;
+
+	/// <summary>
+    /// Runs on start of the scene.
+    /// </summary>
+	void Start () {
+        CurrentTime = InitialTime;
+        Text = GetComponentInChildren<Text>();
+	}
+	
+	/// <summary>
+    /// Runs Once per frame
+    /// </summary>
+	void Update () {
+        if (CurrentTime > 0) {
+            Text.text = ((int)CurrentTime).ToString();
+            CurrentTime = CurrentTime - Time.deltaTime;
+        }
+	}
+}
