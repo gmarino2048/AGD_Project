8512379f426a5fa24d1564642071684ef66aee29
Guy Marino <gxm262@case.edu>
2018-10-01 12:59:43 -0400

    Fixed some scaling issues and added persisting objects

--- Master Project/Assets/Scenes/ScenePicker/ScenePickerBehavior.cs
+++ Master Project/Assets/Scenes/ScenePicker/ScenePickerBehavior.cs
@@ -1,16 +1,36 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
-public class ScenePickerBehavior : MonoBehaviour {
+public class ScenePickerBehavior : MonoBehaviour
+{
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+    [Header("Buttons")]
+
+    [SerializeField]
+    public Button ChoppingButton;
+
+    [SerializeField]
+    public Button CombatButton;
+
+    [SerializeField]
+    public Button DialogueButton;
+
+    [SerializeField]
+    public Button MicrowaveButton;
+
+    [SerializeField]
+    public Button ShakingButton;
+
+
+    private void Start()
+    {
+        
+    }
+
+    private void OnChoppingPressed ()
+    {
+
+    }
 }
