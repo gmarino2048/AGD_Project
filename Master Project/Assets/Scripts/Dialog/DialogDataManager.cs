c200aa1f2bde47f41ebcf2e1422845940261f79d
Noah Crowley <noahwillcrow@gmail.com>
2018-10-03 17:06:46 -0400

    Improve DialogDataManager and implement into scene.

---
+++ Master Project/Assets/Scripts/Dialog/DialogDataManager.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Xml;
+using UnityEngine;
+
+namespace Dialog
+{
+    public class DialogDataManager
+    {
+        private const string _MONSTER_DIALOG_DATA_FILE_PATH = "Data/MonsterDialog.xml";
+
+        /// <summary>
+        /// Gets the response sets.
+        /// </summary>
+        /// <value>The response sets.</value>
+        public Dictionary<string, Response[]> ResponseSets { get; private set; }
+
+        /// <summary>
+        /// Gets the prompts.
+        /// </summary>
+        /// <value>The prompts.</value>
+        public Dictionary<string, Prompt> Prompts { get; private set; }
+
+        /// <summary>
+        /// Gets the initial prompt ID.
+        /// </summary>
+        public string InitialPromptID { get; private set; }
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="T:Dialog.DialogDataManager"/> class.
+        /// </summary>
+        private DialogDataManager() {
+
+        }
+
+        /// <summary>
+        /// Creates a new DialogDataManager based on the data stored in the XML file.
+        /// </summary>
+        /// <returns>The new DialogDataManager</returns>
+        /// <param name="monsterId">Monster identifier.</param>
+        public static DialogDataManager LoadFromXml(Guid monsterId) {
+            var xmlAsset = new TextAsset();
+            xmlAsset = (TextAsset)Resources.Load(_MONSTER_DIALOG_DATA_FILE_PATH, typeof(TextAsset));
+
+            var xmlDoc = new XmlDocument();
+            xmlDoc.LoadXml(xmlAsset.text);
+
+            var monsterDialogData = xmlDoc.GetElementById(monsterId.ToString());
+            var dialogDataManager = new DialogDataManager
+            {
+                InitialPromptID = monsterDialogData.Attributes["initialDialog"].Value,
+                Prompts = new Dictionary<string, Prompt>(),
+                ResponseSets = new Dictionary<string, Response[]>()
+            };
+
+            foreach (XmlNode xmlNode in monsterDialogData.GetElementsByTagName("prompt")) {
+                var prompt = new Prompt
+                {
+                    Body = xmlNode.InnerText
+                };
+
+                if (xmlNode.Attributes["nextPrompt"] != null)
+                {
+                    prompt.NextPromptID = xmlNode.Attributes["nextPrompt"].Value;
+                }
+
+                if (xmlNode.Attributes["responseSet"] != null)
+                {
+                    prompt.ResponseSetID = xmlNode.Attributes["responseSet"].Value;
+                }
+
+                if (xmlNode.Attributes["isSaidByPlayer"] != null && bool.Parse(xmlNode.Attributes["isSaidByPlayer"].Value))
+                {
+                    prompt.IsSaidByPlayer = true;
+                }
+
+                dialogDataManager.Prompts[xmlNode.Attributes["id"].Value] = prompt;
+            }
+            
+            foreach (XmlNode xmlNode in monsterDialogData.GetElementsByTagName("responseSet")) {
+                var responses = new List<Response>();
+                foreach (XmlNode responseNode in xmlNode.ChildNodes) {
+                    responses.Add(new Response
+                    {
+                        Body = responseNode.InnerText,
+                        Value = int.Parse(responseNode.Attributes["value"].Value),
+                        NextPromptID = responseNode.Attributes["nextPrompt"].Value
+                    });
+                }
+                dialogDataManager.ResponseSets.Add(xmlNode.Attributes["id"].Value, responses.ToArray());
+            }
+
+            return dialogDataManager;
+        }
+    }
+}
--- Master Project/Assets/Scripts/Dialog/Prompt.cs
+++ Master Project/Assets/Scripts/Dialog/Prompt.cs
@@ -2,6 +2,7 @@
 {
     public class Prompt
     {
+        public bool IsSaidByPlayer { get; set; }
         public string Body { get; set; }
         public string NextPromptID { get; set; }
         public string ResponseSetID { get; set; }
