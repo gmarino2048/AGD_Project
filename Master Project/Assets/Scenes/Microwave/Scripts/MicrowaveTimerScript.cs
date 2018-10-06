65dc7571b3e77d49807f546f58ca318fa6f5d434
markgoldberg <mark.goldberg@gmail.com>
2018-10-06 14:18:04 -0400

    added in mircowave sprite and tutorial sprite/code

--- Master Project/Assets/Scenes/Microwave/Scripts/MicrowaveTimerScript.cs
+++ Master Project/Assets/Scenes/Microwave/Scripts/MicrowaveTimerScript.cs
@@ -19,17 +19,22 @@ public class MicrowaveTimerScript : MonoBehaviour
     /// </summary>
     void Start()
     {
-        counter = 30;
-        //every second call countdown method (starts after a second)
-        InvokeRepeating("Countdown", 1, 1);
-        TimerText.text = "00:" + counter.ToString();
-        stillRunning = true;
+
     }
 
     // Update is called once per frame
     void Update()
     {
 
+    }
+
+    public void StartGame()
+    {
+        counter = 10;
+        //every second call countdown method (starts after a second)
+        InvokeRepeating("Countdown", 1, 1);
+        TimerText.text = "00:" + counter.ToString();
+        stillRunning = true;
     }
 
     /// <summary>
