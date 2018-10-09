d4a0c9ea3e1e10f5d464f3bc158f51ef2133b7f7
markgoldberg <mark.goldberg@gmail.com>
2018-10-09 09:01:52 -0400

    added back in microwave stuff for git

---
+++ Master Project/Assets/Scenes/Microwave/Scripts/Microwave.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Microwave : MonoBehaviour {
+
+    private int counter;
+    public Text TimerText;
+
+	// Use this for initialization
+	void Start () {
+        counter = 300;
+        InvokeRepeating("Countdown", 1, 1);
+        TimerText.text = "00:" + counter.ToString();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+
+    void Countdown()
+    {
+        counter--;
+        TimerText.text = "00:" + counter.ToString();
+    }
+}
---
+++ Master Project/Assets/Scenes/Microwave/Scripts/MicrowaveTimerScript.cs
@@ -0,0 +1,80 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MicrowaveTimerScript : MonoBehaviour
+{
+    //how much time is left
+    private int counter;
+    //textbox with countdown
+    public Text TimerText;
+    //whether the game is still going
+    private bool stillRunning;
+
+    Animator anim;
+
+    /// <summary>
+    /// Start this instance.
+    /// initializes variables
+    /// starts timer
+    /// </summary>
+    void Start()
+    {
+        anim = GameObject.Find("Microwave Sprite").GetComponent<Animator>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+    public void StartGame()
+    {
+        counter = 10;
+        //every second call countdown method (starts after a second)
+        InvokeRepeating("Countdown", 1, 1);
+        TimerText.text = "00:" + counter.ToString();
+        stillRunning = true;
+    }
+
+    /// <summary>
+    /// assuming game hasn't finished, decrements counter
+    /// also updates textbox
+    /// </summary>
+    void Countdown()
+    {
+        if (counter > 0)
+        {
+            if (stillRunning == true)
+            {
+                counter--;
+                TimerText.text = "00:" + counter.ToString();
+            }
+        }
+        else{
+            FinishMicrowaveGame();
+        }
+        
+    }
+
+    /// <summary>
+    /// if GUI button to stop microwave is clciked
+    /// stops timer
+    /// stops game
+    /// </summary>
+   public void buttonClicked(){
+        stillRunning = false;
+        anim.SetTrigger("Open");
+        FinishMicrowaveGame();
+    }
+
+    /// <summary>
+    /// after game is done, this is called
+    /// not sure what to do here yet
+    /// </summary>
+    private void FinishMicrowaveGame(){
+
+    }
+}
