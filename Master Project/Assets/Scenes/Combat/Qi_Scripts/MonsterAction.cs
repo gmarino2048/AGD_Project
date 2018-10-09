ad92659f9cda88efee850dfe266ec5fba0bc0bb7
riyuezaozao <qli43@stevens.edu>
2018-10-08 23:40:40 -0400

    BattleScene
    Add Monster animation when hitted. Add health bar and manager bar number display. Change the UI to the newest version

--- Master Project/Assets/Scenes/Combat/Qi_Scripts/MonsterAction.cs
+++ Master Project/Assets/Scenes/Combat/Qi_Scripts/MonsterAction.cs
@@ -1,9 +1,15 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class MonsterAction : MonoBehaviour {
 
     public enum Monster {Nessie, Cerberus, REDACTED};
+    public Animator Hitted;
 
+    public void MonsterHitted()
+    {
+        Hitted.SetTrigger("MonsterHitted");
+    }
 }
