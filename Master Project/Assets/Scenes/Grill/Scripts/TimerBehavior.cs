64b9653d278dc025630fd516b55ed90b62c4b9c4
Guy Marino <gxm262@case.edu>
2018-11-07 20:36:37 -0500

    UI Changes and Timer update

---
+++ Master Project/Assets/Scenes/Grill/Scripts/TimerBehavior.cs
@@ -0,0 +1,52 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+namespace Grill
+{
+    public class TimerBehavior : MonoBehaviour
+    {
+
+        [Header("Timer Text")]
+        public string Message = "Time Remaining:";
+        public Text TimerText;
+
+        [Header("Initial Time")]
+        public int InitialTime = 30;
+        public float TimeRemaining { get; private set; }
+
+        public bool GameActive { get; private set; }
+        public bool GameOver { get; private set; }
+
+        // Use this for initialization
+        void Start()
+        {
+            TimeRemaining = InitialTime;
+            GameActive = false;
+            GameOver = false;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            if (GameActive && TimeRemaining > 0)
+            {
+                TimeRemaining -= Time.deltaTime;
+                int currentTime = Mathf.RoundToInt(TimeRemaining);
+                string toDisplay = Message + " " + currentTime.ToString();
+
+                TimerText.text = toDisplay;
+            }
+            else if (!GameOver && GameActive)
+            {
+                GameActive = false;
+                GameOver = true;
+            }
+        }
+
+        public void Activate () {
+            GameActive = true;
+        }
+    }
+}
