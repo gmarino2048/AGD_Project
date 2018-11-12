f4162e15f907f1666ecf9b5fd5f858b25ac36650
Guy Marino <gxm262@case.edu>
2018-11-11 21:53:49 -0500

    More work done on Grill

--- Master Project/Assets/Scenes/Grill/Scripts/PlacementController.cs
+++ Master Project/Assets/Scenes/Grill/Scripts/PlacementController.cs
@@ -9,8 +9,12 @@ namespace Grill
 
         [Header("Cook Object Prefab")]
         public CookObjectController CookObject;
+        public float ZIndex = -1;
 
-        List<CookObjectController> Active;
+        [Header("Game Controller")]
+        public TimerBehavior GameController;
+
+        public List<CookObjectController> Active { get; private set; }
 
         Camera MainCamera;
 
@@ -18,18 +22,48 @@ namespace Grill
         void Start()
         {
             MainCamera = Camera.main;
+            Active = new List<CookObjectController>();
         }
 
         // Update is called once per frame
         void Update()
         {
-            if (Input.GetMouseButtonDown(0))
+            if (GameController.GameActive && Input.GetMouseButtonDown(0))
             {
                 Vector3 mousePosition = Input.mousePosition;
                 Vector3 worldPosition = MainCamera.ScreenToWorldPoint(mousePosition);
 
+                CookObjectController instantiated = Instantiate(CookObject);
 
+                if (CheckPosition(instantiated.gameObject))
+                {
+                    Active.Add(instantiated);
+
+                    instantiated.transform.position =
+                        new Vector3(worldPosition.x, worldPosition.y, ZIndex);
+                }
+                else DestroyImmediate(instantiated);
             }
+        }
+
+        bool CheckPosition (GameObject instantiated) 
+        {
+            Collider2D thisCollider = instantiated.GetComponent<Collider2D>();
+
+            bool valid = true;
+
+            if (thisCollider != null)
+            {
+                foreach (CookObjectController cookObject in Active)
+                {
+                    Collider2D other = cookObject.gameObject.GetComponent<Collider2D>();
+
+                    valid = thisCollider.IsTouching(other) ? valid && false : valid && true;
+                }
+            }
+            else valid = false;
+
+            return valid;
         }
     }
 }
