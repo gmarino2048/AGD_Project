dee27a216f63adf493950b8179b4dede974f7fb1
Guy Marino <gxm262@case.edu>
2018-11-12 17:43:19 -0500

    Changes to grill objects

--- Master Project/Assets/Scenes/Grill/Scripts/CookObjectController.cs
+++ Master Project/Assets/Scenes/Grill/Scripts/CookObjectController.cs
@@ -27,17 +27,23 @@ namespace Grill
         [Header("Game Controls")]
         public TimerBehavior GameController;
         public BoxCollider2D MainCollider;
-
+        public Camera MainCamera;
+        
+        public float InternalTimer { get; private set; }
         public float Score { get; private set; }
 
         // Use this for initialization
         void Start()
         {
+            MainCamera = Camera.main;
+
             CookTime = TimeToCook;
             BurnTime = TimeToBurn;
 
             PreviousState = "";
             OriginalSpeed = Controller.speed;
+
+            InternalTimer = 0f;
         }
 
         // Update is called once per frame
@@ -47,28 +53,60 @@ namespace Grill
 
             if (GameController.GameActive)
             {
-                if (info.IsName(CookingStateName) && PreviousState != CookingStateName)
-                {
-                    Controller.speed = OriginalSpeed;
-                    float newSpeed = Controller.speed / CookTime;
-                    Controller.speed = newSpeed;
+                UpdateAnimation(info);
 
-                    PreviousState = CookingStateName;
+                if (Input.GetMouseButtonDown(0))
+                {
+                    Vector3 mousePosition = Input.mousePosition;
+                    Vector3 worldPosition = MainCamera.ScreenToWorldPoint(mousePosition);
+
+                    if (MainCollider.bounds.Contains((Vector2)worldPosition)) RemovePatty();
                 }
 
-                if (info.IsName(BurningStateName) && PreviousState != BurningStateName)
-                {
-                    Controller.speed = OriginalSpeed;
-                    float newSpeed = Controller.speed / BurnTime;
-                    Controller.speed = newSpeed;
-
-                    PreviousState = BurningStateName;
-                }
+                InternalTimer += Time.deltaTime;
             }
             else {
                 Controller.speed = 0;
                 PreviousState = "";
             }
+        }
+
+        void UpdateAnimation (AnimatorStateInfo info) 
+        {
+            if (info.IsName(CookingStateName) && PreviousState != CookingStateName)
+            {
+                Controller.speed = OriginalSpeed;
+                float newSpeed = Controller.speed / CookTime;
+                Controller.speed = newSpeed;
+
+                PreviousState = CookingStateName;
+            }
+
+            if (info.IsName(BurningStateName) && PreviousState != BurningStateName)
+            {
+                Controller.speed = OriginalSpeed;
+                float newSpeed = Controller.speed / BurnTime;
+                Controller.speed = newSpeed;
+
+                PreviousState = BurningStateName;
+            }
+        }
+
+        void RemovePatty () {
+            Score = CalculateScore();
+        }
+
+        float CalculateScore () {
+            if (InternalTimer <= CookTime)
+            {
+                return 1f - (InternalTimer / CookTime);
+            }
+            if (InternalTimer > CookTime && InternalTimer <= CookTime + BurnTime)
+            {
+                float scaledTime = InternalTimer - CookTime;
+                return scaledTime / BurnTime;
+            }
+            return 1f;
         }
     }
 }
