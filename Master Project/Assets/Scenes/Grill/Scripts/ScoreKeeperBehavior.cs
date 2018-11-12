b24282d53f4e125adcf2587c307f7a5d7f47b508
Guy Marino <gxm262@case.edu>
2018-11-12 17:41:26 -0500

    Updates to Grill Scorekeeper

--- Master Project/Assets/Scenes/Grill/Scripts/ScoreKeeperBehavior.cs
+++ Master Project/Assets/Scenes/Grill/Scripts/ScoreKeeperBehavior.cs
@@ -2,20 +2,53 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ScoreKeeperBehavior : MonoBehaviour {
+namespace Grill
+{
+    public class ScoreKeeperBehavior : MonoBehaviour
+    {
 
-    [Header("Game Controls")]
+        [Header("Game Controls")]
+        public TimerBehavior Timer;
 
-    [Header("UI Stuff")]
-    public CanvasGroup ScoreDisplay;
+        [Header("Score Settings")]
+        public uint CookTarget;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [Header("UI Stuff")]
+        public CanvasManager Manager;
+        public CanvasGroup ScoreDisplay;
+
+        public uint Cooked { get; private set; }
+        float AverageScore;
+
+        public float Score { get; private set; }
+
+
+        // Use this for initialization
+        void Start()
+        {
+            Score = 1f;
+            AverageScore = -1f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+
+        }
+
+        public void AddScore (CookObjectController cookObject)
+        {
+            if (AverageScore < 0)
+            {
+                AverageScore = cookObject.Score;
+                Cooked++;
+            }
+            else
+            {
+                float tempScore = AverageScore * Cooked;
+                tempScore += cookObject.Score;
+                AverageScore = tempScore / ++Cooked;
+            }
+        }
+    }
 }
