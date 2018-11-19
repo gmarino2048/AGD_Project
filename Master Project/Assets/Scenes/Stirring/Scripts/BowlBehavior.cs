3e285c80cf0257ef326d2eb76f399d2f2fa142e6
Guy Marino <gxm262@case.edu>
2018-11-19 00:14:17 -0500

    Sorta updated animation controller... doesnt exactly work...

--- Master Project/Assets/Scenes/Stirring/Scripts/BowlBehavior.cs
+++ Master Project/Assets/Scenes/Stirring/Scripts/BowlBehavior.cs
@@ -2,15 +2,58 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BowlBehavior : MonoBehaviour {
+namespace Stirring
+{
+    public class BowlBehavior : MonoBehaviour
+    {
+        [Header("Game Controllers")]
+        public TimerBehavior Timer;
+        public Animator BowlAnimator;
+        public SpoonBehavior Spoon;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
-	
-	// Update is called once per frame
-	void Update () {
-		
-	}
+        [Header("Bowl Settings")]
+        public float Scaler = 2.25f;
+        float DirectionMultiplier;
+
+        [Header("Animator Parameters")]
+        public string IngredientSelector;
+        public string TransitionTrigger;
+
+        public float Acceleration = 0.25f;
+        float Speed;
+        public float MaxSpeed;
+
+        // Use this for initialization
+        void Start()
+        {
+            Speed = 0f;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            // Set animator speed
+            if (Speed >= 0) Speed += Spoon.Dragging ?
+                                          (Spoon.Direction ? Acceleration * Time.deltaTime : -2 * Acceleration * Time.deltaTime) :
+                                          (-1 * Acceleration * Time.deltaTime);
+            else Speed += Spoon.Dragging ?
+                               (Spoon.Direction ? 2 * Acceleration * Time.deltaTime : -1 * Acceleration * Time.deltaTime) :
+                               (Acceleration * Time.deltaTime);
+
+            Speed = Mathf.Clamp(Speed, -1 * MaxSpeed, MaxSpeed);
+
+            DirectionMultiplier = Speed >= 0 ? 1 : -1;
+
+            float animSpeed = Mathf.Abs(Speed);
+
+            BowlAnimator.speed = animSpeed;
+
+            float xScale = Scaler * DirectionMultiplier;
+            float yScale = xScale;
+
+            gameObject.transform.localScale = new Vector3(xScale, yScale, 1);
+
+
+        }
+    }
 }
