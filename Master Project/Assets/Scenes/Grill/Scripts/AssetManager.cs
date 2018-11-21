e4513051b0251c958c8222353e7be004594ec9d1
Guy Marino <gxm262@case.edu>
2018-11-21 16:33:31 -0500

    Asset manager script done

---
+++ Master Project/Assets/Scenes/Grill/Scripts/AssetManager.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using UnityEngine;
+
+namespace Grill
+{
+    public class AssetManager : MonoBehaviour
+    {
+
+        [Header("Beef Assets")]
+        public Sprite UncookedPatty;
+        public CookObjectController BeefPrefab;
+
+
+        [Header("Steak Assets")]
+        public Sprite UncookedSteak;
+        public CookObjectController SteakPrefab;
+
+        [Header("Scene Objects")]
+        public SpriteRenderer GrillPlacementHelper;
+        public PlacementController GrillPlacementController;
+
+        void Awake()
+        {
+            DishPreparationManager dishManager = FindObjectOfType<DishPreparationManager>();
+
+            try 
+            {
+                IngredientType ingredient = dishManager.currentIngredient;
+
+                switch (ingredient) 
+                {
+                    case IngredientType.GroundBeef:
+                        SetParameters(UncookedPatty, BeefPrefab);
+                        break;
+
+                    case IngredientType.Steak:
+                        SetParameters(UncookedSteak, SteakPrefab);
+                        break;
+
+                    default:
+                        Debug.LogError("Wrong ingredient type -- defaulting to beef");
+                        SetParameters(UncookedPatty, BeefPrefab);
+                        break;
+                }
+            }
+            catch (Exception ex)
+            {
+                Debug.LogError(ex.Message + " -- defaulting to beef");
+                SetParameters(UncookedSteak, SteakPrefab);
+            }
+        }
+
+        void SetParameters(Sprite PlacementHelper, CookObjectController Prefab)
+        {
+            GrillPlacementHelper.sprite = PlacementHelper;
+            GrillPlacementController.CookObject = Prefab;
+        }
+    }
+}
