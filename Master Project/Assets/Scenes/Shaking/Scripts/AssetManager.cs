e2a552e593e1dd066fc810f2c5c89bd696b880ba
Guy Marino <gxm262@case.edu>
2018-11-22 02:20:25 -0500

    Started work on asset changing

---
+++ Master Project/Assets/Scenes/Shaking/Scripts/AssetManager.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using UnityEngine;
+
+namespace Shaking
+{
+    public class AssetManager : MonoBehaviour
+    {
+
+        [Header("Gravel Assets")]
+        public string GravelTrigger = "GravelTrigger";
+        public Sprite GravelShaker;
+
+        [Header("Souls Assets")]
+        public string SoulsTrigger = "SoulsTrigger";
+        public Sprite SoulsShaker;
+
+        [Header("Scene Objects")]
+        public SpriteRenderer ShakerSprite;
+        public ShakerBehavior Shaker;
+
+        void Awake()
+        {
+            DishPreparationManager dishManager = FindObjectOfType<DishPreparationManager>();
+
+            try 
+            {
+                IngredientType ingredient = dishManager.currentIngredient;
+
+                switch (ingredient)
+                {
+                    case IngredientType.AquariumGravel:
+                        SetParameters(GravelTrigger, GravelShaker);
+                        break;
+
+                    case IngredientType.CrushedSouls:
+                        SetParameters(SoulsTrigger, SoulsShaker);
+                        break;
+
+                    default:
+                        Debug.LogError("Ingredient not found -- defaulting to Gravel");
+                        SetParameters(GravelTrigger, GravelShaker);
+                        break;
+                }
+            }
+            catch (Exception ex) 
+            {
+                Debug.LogError(ex.Message + " -- defaulting to Gravel");
+                SetParameters(GravelTrigger, GravelShaker);
+            }
+        }
+
+        void SetParameters (string TriggerName, Sprite ShakerSet) 
+        {
+            ShakerSprite.sprite = ShakerSet;
+            Shaker.StartTrigger = TriggerName;
+        }
+    }
+}
