c82de8410c04568a818caadc2179c13470c7197a
Guy Marino <gxm262@case.edu>
2018-11-23 18:55:01 -0500

    Namespace fixed for chopping

--- Master Project/Assets/Scenes/Chopping/Scripts/SFXController.cs
+++ Master Project/Assets/Scenes/Chopping/Scripts/SFXController.cs
@@ -2,17 +2,21 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class SFXController : MonoBehaviour {
-
-    [Header("Audio Source")]
-    public AudioSource SFXPlayer;
-    public float SFXScale;
-
-    [Header("Audio Clips")]
-    public AudioClip KnifeCut;
-
-    public void PlayCut () 
+namespace Chopping
+{
+    public class SFXController : MonoBehaviour
     {
-        SFXPlayer.PlayOneShot(KnifeCut, SFXScale);
+
+        [Header("Audio Source")]
+        public AudioSource SFXPlayer;
+        public float SFXScale;
+
+        [Header("Audio Clips")]
+        public AudioClip KnifeCut;
+
+        public void PlayCut()
+        {
+            SFXPlayer.PlayOneShot(KnifeCut, SFXScale);
+        }
     }
 }
