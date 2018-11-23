6c304535c6513ac749ecb9467d84b2398275e87c
Guy Marino <gxm262@case.edu>
2018-11-23 18:52:29 -0500

    Creating SFX controller

---
+++ Master Project/Assets/Scenes/Grill/Scripts/SFXController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Grill
+{
+    public class SFXController : MonoBehaviour
+    {
+
+        [Header("Audio Settings")]
+        public AudioSource SFXPlayer;
+        public float SFXScaler;
+
+        [Header("Sound Effect Clips")]
+        public AudioClip Sizzle;
+        public AudioClip Spatula;
+
+
+        public void PlaySizzle()
+        {
+            SFXPlayer.PlayOneShot(Sizzle, SFXScaler);
+        }
+
+        public void PlaySpatula()
+        {
+
+        }
+    }
+}
