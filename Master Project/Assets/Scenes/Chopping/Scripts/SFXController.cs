62d9427ef43bd1b66509d5041763269a111087e7
Guy Marino <gxm262@case.edu>
2018-11-23 18:43:26 -0500

    Knife chop sound now in chopping scene

---
+++ Master Project/Assets/Scenes/Chopping/Scripts/SFXController.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SFXController : MonoBehaviour {
+
+    [Header("Audio Source")]
+    public AudioSource SFXPlayer;
+    public float SFXScale;
+
+    [Header("Audio Clips")]
+    public AudioClip KnifeCut;
+
+    public void PlayCut () 
+    {
+        SFXPlayer.PlayOneShot(KnifeCut, SFXScale);
+    }
+}
