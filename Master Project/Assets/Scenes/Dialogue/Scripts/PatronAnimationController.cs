362915df1c3d64a57f1a90d7f0c74d922d39593b
markgoldberg <mark.goldberg@gmail.com>
2018-11-25 15:04:44 -0500

    working on getting animation controller working

--- Master Project/Assets/Scenes/Dialogue/Scripts/PatronAnimationController.cs
+++ Master Project/Assets/Scenes/Dialogue/Scripts/PatronAnimationController.cs
@@ -4,13 +4,40 @@ using UnityEngine;
 
 public class PatronAnimationController : MonoBehaviour {
 
+    //which monster
+    public string monsterType;
+
+    //animator next to patron
+    public Animator patronAnimator;
+
 	// Use this for initialization
 	void Start () {
-		
-	}
+        monsterType = "Nessie";
+        patronAnimator.SetTrigger("Nessie");
+    }
 	
 	// Update is called once per frame
 	void Update () {
 		
 	}
+
+    void Neutral(){
+        patronAnimator.SetTrigger("Neutral");
+    }
+
+    void Happy()
+    {
+        patronAnimator.SetTrigger("Happy");
+    }
+
+    void Sad()
+    {
+        patronAnimator.SetTrigger("Sad");
+    }
+
+    void End()
+    {
+        patronAnimator.SetTrigger("Ending");
+    }
 }
+
