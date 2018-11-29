69834f64df7e157f2ee1ba5b03e4dd85051f3c45
markgoldberg <mark.goldberg@gmail.com>
2018-11-29 08:43:39 -0500

    tried adding button and failed

---
+++ Master Project/Assets/Scenes/GameOver/GameOverButton.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameOverButton : MonoBehaviour {
+
+    /// <summary>
+    /// The name of the next scene.
+    /// </summary>
+    public string nextSceneName;
+
+    // Update is called once per frame
+    void OnMouseDown()
+    {
+        Debug.Log("grrrr");
+        SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+    }
+
+    public void MyButtonClick()
+    {
+        Debug.Log("grrrr");
+        SceneManager.LoadScene(nextSceneName, LoadSceneMode.Single);
+    }
+}
