07efc533a6c10ab11a84aa7f8aef821997e41afd
Guy Marino <gxm262@case.edu>
2018-11-28 20:13:48 -0500

    Added Nessie Animations

---
+++ Master Project/Assets/Scenes/Dialogue/Assets/Monsters/Nessie/NessieController.cs
@@ -0,0 +1,69 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialogue
+{
+    public class NessieController : MonsterController
+    {
+        [Header("Neutral Trigger")]
+        public string NeutralTrigger;
+
+        [Header("Bob animation")]
+        public GameObject NessieSprite;
+        public float Range;
+        public float Boundary;
+        public float BobDuration;
+
+        float StartTime;
+
+        bool Bobbing;
+        bool Direction; // Sprite is bobbing up
+
+        float Top;
+        float Bottom;
+
+        void Start()
+        {
+            Top = NessieSprite.transform.position.y + Range;
+            Bottom = NessieSprite.transform.position.y - Range;
+
+            StartTime = Time.time;
+            Direction = true;
+
+            Bobbing = true;
+            StartCoroutine(NeutralAnimate(NeutralTrigger));
+        }
+
+        private void Update()
+        {
+            Vector3 spritePosition = NessieSprite.transform.position;
+
+            float yPosition = spritePosition.y;
+            float elapsedTime = Time.time - StartTime;
+            float scaledTime = elapsedTime / BobDuration;
+
+            float destination = Direction ? Top : Bottom;
+            float newY = Mathf.SmoothStep(yPosition, destination, scaledTime);
+            
+
+            if (Mathf.Abs(newY - destination) <= Boundary)
+            {
+                StartTime = Time.time;
+                Direction = !Direction;
+            }
+
+            NessieSprite.transform.position = new Vector3(spritePosition.x, newY, spritePosition.z);
+        }
+
+        IEnumerator NeutralAnimate (string neutralTrigger)
+        {
+            while (Bobbing)
+            {
+                float waitSeconds = Random.Range(5f, 15f);
+                yield return new WaitForSeconds(waitSeconds);
+                Animation.SetTrigger(neutralTrigger);
+            }
+        }
+    }
+}
---
+++ Master Project/Assets/Scenes/Dialogue/Scripts/MonsterController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Dialogue
+{
+    public abstract class MonsterController : MonoBehaviour
+    {
+        [Header("Animator")]
+        public Animator Animation;
+
+        [Header("Trigger Names")]
+        public string GoodTrigger;
+        public string BadTrigger;
+
+        public void TriggerGood ()
+        {
+            Animation.SetTrigger(GoodTrigger);
+        }
+
+        public void TriggerBad ()
+        {
+            Animation.SetTrigger(BadTrigger);
+        }
+    }
+}
