456afbaaa2eb704b32b3fafbb8564b0b63774e78
markgoldberg <mark.goldberg@gmail.com>
2018-12-02 17:14:54 -0500

    made it rotate again

---
+++ Master Project/Assets/Scenes/FoodScoring/Scripts/OverlayScript.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class OverlayScript : MonoBehaviour {
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(0, 0, 1);
+	}
+}
