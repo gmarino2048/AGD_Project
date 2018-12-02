15ab06e1007e98c2985c243b5e51959eec4c4510
markgoldberg <mark.goldberg@gmail.com>
2018-12-02 17:47:29 -0500

    fixed rotating thing and added sound

--- Master Project/Assets/Scenes/FoodScoring/Scripts/ScoreTextScript.cs
+++ Master Project/Assets/Scenes/FoodScoring/Scripts/ScoreTextScript.cs
@@ -8,14 +8,39 @@ namespace FoodScoring {
     public class ScoreTextScript : MonoBehaviour {
         public Text textBox;
 
+        public AudioSource PositiveResult;
+        public AudioSource NegativeResult;
+
         // Use this for initialization
-        void Start () {
+        IEnumerator Start () {
             var gameNarrativeManager = GameObject.FindObjectOfType<GameNarrativeManager>();
             var dishScoreManager = GameObject.FindObjectOfType<DishScoreManager>();
+            var monsterFactory = GameObject.FindObjectOfType<MonsterFactory>();
 
             float score = dishScoreManager == null ? 0.1f : dishScoreManager.ScoreDish(gameNarrativeManager.CurrentStage.MonsterID);
 
             textBox.text = "Score: "+ (1-score)*1000;
+            Debug.Log("how");
+            yield return new WaitForSeconds(1);
+            Debug.Log("....");
+            if (monsterFactory != null){
+                var monster = monsterFactory.LoadMonster(gameNarrativeManager.CurrentStage.MonsterID);
+                if (monster.AffectionValue >= monster.FightThreshold)
+                {
+                    PositiveResult.Play();
+                }
+                else {
+                    NegativeResult.Play();
+                }
+            }
+            else{
+                NegativeResult.Play();
+            }
         }
+
+       /* IEnumerator delay(int amount){
+            Debug.Log("what");
+            yield return new WaitForSeconds(amount);
+        }*/
     }
 }
