7fedd421e777b93548ed122ed98aafc6b59bf66b
Noah Crowley <noahwillcrow@gmail.com>
2018-12-07 10:47:54 -0500

    This might work, Unity will not open for me

--- Master Project/Assets/Scripts/PersistentObject.cs
+++ Master Project/Assets/Scripts/PersistentObject.cs
@@ -3,31 +3,12 @@ using UnityEngine.SceneManagement;
 
 public class PersistentObject : MonoBehaviour
 {
-    private readonly string _MAIN_MENU_SCENE_NAME = "Main Menu";
-
     private bool isPersistent = false;
 
     void Awake()
     {
-        SceneManager.sceneLoaded += OnSceneLoaded;
+        Debug.Log("Persisting object");
+        DontDestroyOnLoad(this.gameObject);
+        isPersistent = true;
     }
-
-	private void OnSceneLoaded(Scene scene, LoadSceneMode loadSceneMode)
-	{
-        if (scene.name == _MAIN_MENU_SCENE_NAME)
-        {
-            if (isPersistent)
-            {
-                Debug.Log("Destroying formerly persistent object");
-                SceneManager.sceneLoaded -= OnSceneLoaded;
-                //DestroyImmediate(this.gameObject);
-            }
-            else
-            {
-                Debug.Log("Persisting object");
-                DontDestroyOnLoad(this.gameObject);
-                isPersistent = true;
-            }
-        }
-	}
 }
